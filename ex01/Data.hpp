89cf27a47a1d59ee6ccdd64306845534038bd7b5
abekri <abekri@student.42heilbronn.de>
2024-12-18 06:23:32 +0100

    set the project as finished

--- ex01/Data.hpp
+++ ex01/Data.hpp
@@ -8,6 +8,8 @@ struct Data {
     std::string name;
     int age;
 
+    //Data(); not need any Orthodox Canonical Form 
+    // used as extra part 
     Data(const std::string& name = "Default", int age = 0);
     Data(const Data& other);
     Data& operator=(const Data& other);
